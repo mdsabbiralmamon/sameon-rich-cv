cee8a11b6884340f427be85ad1ac70bc30985912
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-06-14 23:01:49 +0600

    added nav bar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from "tailwindcss";
-
+import daisyui from 'daisyui'
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -13,6 +13,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [daisyui],
 };
 export default config;
