2e907c2fd39d46e83552fd725c21115e0b86f951
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-06-22 14:19:22 +0600

    margin modified

--- src/components/HomePage/CurrentActivities/CurrentActivities.tsx
+++ src/components/HomePage/CurrentActivities/CurrentActivities.tsx
@@ -149,7 +149,7 @@ export default function CurrentActivities() {
                         }
                     </div>
                 </div>
-                <div className='glass rounded-lg w-full p-8 col-span-3 mt-4'>
+                <div className='glass rounded-lg w-full p-8 col-span-3'>
                     <h2 className='text-3xl font-bold'>Coding Activity (Last 7 days):</h2>
                     <div className='flex flex-col md:flex-row gap-8 justify-between my-8'>
                         <div className='rounded-xl w-full overflow-hidden'>
