7edf85ae4a4482f0f762865e56bbb6da8f5655fb
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-06-22 10:45:39 +0600

    active links system modified

--- src/components/Shared/Navbar/Navbar.tsx
+++ src/components/Shared/Navbar/Navbar.tsx
@@ -1,39 +1,51 @@
+'use client'
+
+import useHash from '@/app/hooks/UseHash';
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 
 export default function Navbar() {
+    const pathname = usePathname();
+    const hash = useHash();
+
+    const newPath = hash !== null ? `${pathname}#${hash}` : pathname;
+    
+    console.log("hash", newPath);
+    
+
     const navLinks = <>
-        <li><Link href={'/'}>Home</Link></li>
-        <li><Link href={'/about'}>About</Link></li>
-        <li><Link href={'/skills'}>Skills</Link></li>
-        <li><Link href={'/experiences'}>Experiences</Link></li>
-        <li><Link href={'/blog'}>Blog</Link></li>
-        <li><Link href={'/contact'}>Contact</Link></li>
+        <li><Link className={`${newPath === '/#onlineStatus' ? 'active' : ''}`} href={'#onlineStatus'}>Status</Link></li>
+        <li><Link className={`${newPath === '/#about' ? 'active' : ''}`} href={'#about'}>About</Link></li>
+        <li><Link className={`${newPath === '/skills' ? 'active' : ''}`} href={'/skills'}>Skills</Link></li>
+        <li><Link className={`${newPath === '/experiences' ? 'active' : ''}`} href={'/experiences'}>Experiences</Link></li>
+        <li><Link className={`${newPath === '/blog' ? 'active' : ''}`} href={'/blog'}>Blog</Link></li>
+        <li><Link className={`${newPath === '/contact' ? 'active' : ''}`} href={'/contact'}>Contact</Link></li>
     </>
     return (
         <div className='bg-gray-800 text-white shadow-xl fixed w-full z-10'>
             <div className="navbar container mx-auto">
-            <div className="navbar-start">
-                <div className="dropdown">
-                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </div>
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-gray-800 text-white rounded-box w-52">
+                            {
+                                navLinks
+                            }
+                        </ul>
                     </div>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-gray-800 text-white rounded-box w-52">
+                    <a href='/' className="btn btn-ghost text-xl">SAMEON</a>
+                </div>
+                <div className="navbar-end hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
                         {
                             navLinks
                         }
                     </ul>
                 </div>
-                <a className="btn btn-ghost text-xl">SAMEON</a>
             </div>
-            <div className="navbar-end hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
-                    {
-                        navLinks
-                    }
-                </ul>
-            </div>
-        </div>
         </div>
     )
 }
