055325ac32f884b2e550f6c13db828a844645826
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-06-22 10:44:05 +0600

    added margin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import HeroSection from "@/components/HomePage/HeroSection/HeroSection";
 export default function Home() {
   return (
     <>
-      <div className="heroSection container mx-auto">
+      <div className="heroSection container mx-auto mt-24">
         <HeroSection />
       </div>
       <div className="current container mx-auto">
