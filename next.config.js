3bb6139008b6b7619f897c0b0a0f1a924b4dae9d
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-06-24 01:43:02 +0600

    added img bb domain

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     images: {
-      domains: ['cdn0.iconfinder.com', 'cdn1.iconfinder.com', 'cdn2.iconfinder.com', 'cdn3.iconfinder.com', 'cdn4.iconfinder.com', ],
+      domains: ['cdn0.iconfinder.com', 'cdn1.iconfinder.com', 'cdn2.iconfinder.com', 'cdn3.iconfinder.com', 'cdn4.iconfinder.com', 'i.ibb.co', ],
     },
   }
