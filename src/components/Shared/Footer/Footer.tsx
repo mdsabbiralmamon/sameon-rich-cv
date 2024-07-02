c8e2eddd9cbbff6817f22203deab1f543d45a71d
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-07-02 20:35:15 +0600

    Add: Created footer comp

---
+++ src/components/Shared/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <div className='glass text-center p-4'>
+            <p>Copyright © {new Date().getFullYear()} - All right reserved by Md. Sabbir Al Mamon</p>
+        </div>
+    )
+}
